7dcad5d798fb8279c4a964c7184189163d48449e
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-25 15:40:53 +0500

    add form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyInput from "./companent/input/form";
+
+function App() {
+  return (
+    <div className="App">
+      <MyInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/companent/input/form/index.jsx
@@ -0,0 +1,13 @@
+function MyInput() {
+  return (
+    <div className="form__wrapper">
+      <form>
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="describe" />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  );
+}
+
+export default MyInput;
